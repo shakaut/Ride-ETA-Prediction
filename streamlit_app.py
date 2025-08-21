0b8d5c69d1beb0d1a01a7db831bc3f5086be8eeb
Shakaut Hassain <135606975+shakaut@users.noreply.github.com>
2025-08-21 21:39:23 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import numpy as np
 model = joblib.load('ride_eta_model.pkl')
 encoder = joblib.load('encoder.pkl')
 
-st.subheader('Ride ETA Prediction App')
+st.subheader('Ride ETA (estimated time of arrival) Prediction App')
 
 distance = st.number_input('Distance (km)', min_value=0.5, max_value=15.0, step=0.5)
 traffic = st.selectbox('Traffic', ['low', 'medium', 'high'])
@@ -21,5 +21,6 @@ if st.button('Predict ETA'):
     # Enforce realistic minimum at prediction time too
     min_possible = distance * 2.5
     prediction = max(prediction, min_possible)
+
 
     st.success(f'Estimated Time of Arrival: {round(prediction, 1)} minutes')
